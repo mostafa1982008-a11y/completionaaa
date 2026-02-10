e4661cc757363130ef6b8404d42764e95e92d0a3
mostafa1982008-a11y <mostafa1982008@gmail.com>
2026-02-10 20:41:36 +0200

    Initialize Firestore database in App.tsx

--- App.tsx
+++ App.tsx
@@ -34,7 +34,7 @@ const firebaseConfig = {
 };
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
-
+const db = getFirestore(app);
 async function getUserIP() {
   const res = await fetch('https://api.ipify.org?format=json');
   const data = await res.json();
