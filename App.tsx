6d0a895e19d7c5ba60837ce753d36d0c29262027
mostafa1982008-a11y <mostafa1982008@gmail.com>
2026-02-10 20:34:04 +0200

    Import Firestore functions from Firebase

--- App.tsx
+++ App.tsx
@@ -1,3 +1,4 @@
+import { getFirestore, doc, getDoc, setDoc, serverTimestamp } from "firebase/firestore";
 import React, { useState, useEffect } from 'react';
 
 import { initializeApp } from "firebase/app";
